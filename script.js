f48ca7f7cb0597d49ab06164b636f26c8f62cd55
Ritika-patel <72975134+Ritika-patel@users.noreply.github.com>
2021-05-18 23:38:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload =()=>{
+    setTimeout(()=>{
+        document.querySelector("body").classList.add("display");
+    },400)
+};
+
+
+
+document.querySelector(".hamburger-menu").
+    addEventListener("click", () => {
+        document.querySelector('.container').classList.toggle("change");
+
+    });
+
+document.querySelector(".scroll-btn").addEventListener
+    ("click", () => {
+        document.querySelector("html").style.scrollBehavior
+            = "smooth";
+        setTimeout(() => {
+            document.querySelector("html").style.scrollBehavior
+                = "unset";
+        },1000)
+    })
